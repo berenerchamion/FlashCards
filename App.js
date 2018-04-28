0edd4df0554f595da50e4f1c07c463ba45ba28b3
Beren Erchamion <beren@houseofbeor.net>
2018-04-28 10:02:18 -0400

    Adding the basics.

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from 'react-native'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +17,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+export default class App extends React.Component {
+
+  componentDidMount(){
+    console.log("Hey there...")
+    debugger
+    console.log("This is pretty cool...")
+  }
+
+  render() {
+    return (
+      <Provider store={{creaetStore(reducer)}}>
+      </Provider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    )
+  }
+}
+
